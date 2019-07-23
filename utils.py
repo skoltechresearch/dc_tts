d8059f7f37f89fcd4262e5e98c2b5001e366fe50
Leggerla <52498629+Leggerla@users.noreply.github.com>
2019-07-23 20:46:32 +0300

    Temporarily

--- utils.py
+++ utils.py
@@ -47,22 +47,22 @@ def get_spectrograms(fpath):
     mag = np.abs(linear)  # (1+n_fft//2, T)
 
     # mel spectrogram
-    mel_basis = librosa.filters.mel(hp.sr, hp.n_fft, hp.n_mels)  # (n_mels, 1+n_fft//2)
-    mel = np.dot(mel_basis, mag)  # (n_mels, t)
+    #mel_basis = librosa.filters.mel(hp.sr, hp.n_fft, hp.n_mels)  # (n_mels, 1+n_fft//2)
+    #mel = np.dot(mel_basis, mag)  # (n_mels, t)
 
     # to decibel
-    mel = 20 * np.log10(np.maximum(1e-5, mel))
+    #mel = 20 * np.log10(np.maximum(1e-5, mel))
     mag = 20 * np.log10(np.maximum(1e-5, mag))
 
     # normalize
-    mel = np.clip((mel - hp.ref_db + hp.max_db) / hp.max_db, 1e-8, 1)
+    #mel = np.clip((mel - hp.ref_db + hp.max_db) / hp.max_db, 1e-8, 1)
     mag = np.clip((mag - hp.ref_db + hp.max_db) / hp.max_db, 1e-8, 1)
 
     # Transpose
-    mel = mel.T.astype(np.float32)  # (T, n_mels)
+    #mel = mel.T.astype(np.float32)  # (T, n_mels)
     mag = mag.T.astype(np.float32)  # (T, 1+n_fft//2)
 
-    return mel, mag
+    return mag
 
 def spectrogram2wav(mag):
     '''# Generate wave file from linear magnitude spectrogram
@@ -149,15 +149,15 @@ def load_spectrograms(fpath):
     and extracts spectrograms'''
 
     fname = os.path.basename(fpath)
-    mel, mag = get_spectrograms(fpath)
-    t = mel.shape[0]
+    mag = get_spectrograms(fpath)
+    t = mag.shape[0]
 
     # Marginal padding for reduction shape sync.
     num_paddings = hp.r - (t % hp.r) if t % hp.r != 0 else 0
-    mel = np.pad(mel, [[0, num_paddings], [0, 0]], mode="constant")
+    #mel = np.pad(mel, [[0, num_paddings], [0, 0]], mode="constant")
     mag = np.pad(mag, [[0, num_paddings], [0, 0]], mode="constant")
 
     # Reduction
-    mel = mel[::hp.r, :]
-    return fname, mel, mag
+    #mel = mel[::hp.r, :]
+    return mag
 
